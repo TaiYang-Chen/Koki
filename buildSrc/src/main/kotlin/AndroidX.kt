56b98283710503423af3a3ab5f352d0fed05d517
‘user’ <1563011912@qq.com>
2024-03-23 21:08:54 +0800

    [#1]依赖管理

---
+++ buildSrc/src/main/kotlin/AndroidX.kt
@@ -0,0 +1,17 @@
+/**
+ * Author: Chen
+ * Email: 1563011912@qq.com
+ * Create Date: 2024/3/23
+ * Profile:
+ */
+object AndroidX {
+    val core = "androidx.core:core-ktx:1.9.0"
+    val appcompat = "androidx.appcompat:appcompat:1.6.1"
+    val constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+
+    object junit {
+        val junit = "junit:junit:4.13.2"
+        val testExt = "androidx.test.ext:junit:1.1.5"
+        val testEspresso = "androidx.test.espresso:espresso-core:3.5.1"
+    }
+}
---
+++ buildSrc/src/main/kotlin/Google.kt
@@ -0,0 +1,9 @@
+/**
+ * Author: Chen
+ * Email: 1563011912@qq.com
+ * Create Date: 2024/3/23
+ * Profile:
+ */
+object Google {
+    val material = "com.google.android.material:material:1.8.0"
+}
