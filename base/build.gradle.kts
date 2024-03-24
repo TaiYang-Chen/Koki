def46e7439f9999a2dde7dfa8045797de7a51e94
‘user’ <1563011912@qq.com>
2024-03-24 20:49:05 +0800

    [#3] base的依赖管理

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.chen.base"
-    compileSdk = 33
+    namespace = Environment.namespaces.base
+    compileSdk = Environment.compileSdk
 
     defaultConfig {
-        minSdk = 25
+        minSdk = Environment.minSdk
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = Environment.testInstrumentationRunner
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -38,10 +39,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(AndroidX.core)
+    implementation(AndroidX.appcompat)
+    implementation(AndroidX.constraintlayout)
+    implementation(Google.material)
+
+    testImplementation(AndroidX.junit.junit)
+    androidTestImplementation(AndroidX.junit.testExt)
+    androidTestImplementation(AndroidX.junit.testEspresso)
 }
