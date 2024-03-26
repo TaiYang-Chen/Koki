92b4444bc60397fe555760aa1de063f2c80909c9
‘user’ <1563011912@qq.com>
2024-03-27 00:02:42 +0800

    [#3] 创建application并应用

---
+++ base/src/main/java/com/chen/base/BaseApp.kt
@@ -0,0 +1,12 @@
+package com.chen.base
+
+import android.app.Application
+
+/**
+ * Author: Chen
+ * Email: 1563011912@qq.com
+ * Create Date: 2024/3/26
+ * Profile:
+ */
+open class BaseApp : Application() {
+}
