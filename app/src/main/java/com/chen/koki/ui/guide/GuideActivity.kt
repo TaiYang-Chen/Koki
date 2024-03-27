4865517cd9bd931317482372fe581a66c9169db5
‘user’ <1563011912@qq.com>
2024-03-28 00:16:05 +0800

    [#3] 修改布局

--- app/src/main/java/com/chen/koki/ui/guide/GuideActivity.kt
+++ app/src/main/java/com/chen/koki/ui/guide/GuideActivity.kt
@@ -1,10 +1,7 @@
 package com.chen.koki.ui.guide
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import androidx.viewpager2.widget.ViewPager2
 import com.chen.base.activity.BaseActivity
 import com.chen.base.ext.fixSystemUI
