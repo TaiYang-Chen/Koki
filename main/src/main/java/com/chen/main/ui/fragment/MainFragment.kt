05c7e698b9c8b643ca16be9c21733439492cca6e
‘user’ <1563011912@qq.com>
2024-04-01 23:21:11 +0800

    [#7] 布局结构

---
+++ main/src/main/java/com/chen/main/ui/fragment/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.chen.main.ui.fragment
+
+import android.os.Bundle
+import com.chen.base.fragment.BaseFragment
+import com.chen.base.vm.BaseViewModel
+import com.chen.main.databinding.FragmentMainBinding
+
+/**
+ * create by chenta on 2023/10/20
+ */
+class MainFragment : BaseFragment<BaseViewModel, FragmentMainBinding>() {
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+}
