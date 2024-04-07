1e4f7ab591f12b9d5207de54e559488b9865ed0a
‘user’ <1563011912@qq.com>
2024-04-07 21:22:28 +0800

    [#7] 导入refresh模块

---
+++ refresh-drawable-path/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = Environment.namespaces.refresh_drawable_path
+    compileSdk = Environment.compileSdk
+
+    defaultConfig {
+        minSdk = Environment.minSdk
+
+        testInstrumentationRunner = Environment.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        debug {
+            buildConfigField("boolean", "isRelease", Environment.isRelease.toString())
+        }
+        release {
+            buildConfigField("boolean", "isRelease", Environment.isRelease.toString())
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures{
+        dataBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(AndroidX.core)
+    implementation(AndroidX.appcompat)
+    implementation(AndroidX.constraintlayout)
+    implementation(Google.material)
+
+    testImplementation(AndroidX.junit.junit)
+    androidTestImplementation(AndroidX.junit.testExt)
+    androidTestImplementation(AndroidX.junit.testEspresso)
+
+    implementation(ThirdParty.arouterApi)
+
+    implementation(AndroidX.navigationFragment)
+    implementation(AndroidX.navigationUI)
+    implementation(AndroidX.navigationRuntime)
+
+    api(project(mapOf("path" to ":refresh-drawable-paint")))
+}
---
+++ refresh-header/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = Environment.namespaces.refresh_header
+    compileSdk = Environment.compileSdk
+
+    defaultConfig {
+        minSdk = Environment.minSdk
+
+        testInstrumentationRunner = Environment.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        debug {
+            buildConfigField("boolean", "isRelease", Environment.isRelease.toString())
+        }
+        release {
+            buildConfigField("boolean", "isRelease", Environment.isRelease.toString())
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures{
+        dataBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    compileOnly(project(mapOf("path" to ":refresh-layout")))
+    compileOnly(project(mapOf("path" to ":refresh-drawable-path")))
+    compileOnly(project(mapOf("path" to ":refresh-header-material")))
+    compileOnly(project(mapOf("path" to ":refresh-layout-kernel")))
+    compileOnly(project(mapOf("path" to ":refresh-header-falsify")))
+
+    implementation(AndroidX.core)
+    implementation(AndroidX.appcompat)
+    implementation(AndroidX.constraintlayout)
+    implementation(Google.material)
+
+    testImplementation(AndroidX.junit.junit)
+    androidTestImplementation(AndroidX.junit.testExt)
+    androidTestImplementation(AndroidX.junit.testEspresso)
+
+    implementation(ThirdParty.arouterApi)
+
+    implementation(AndroidX.navigationFragment)
+    implementation(AndroidX.navigationUI)
+    implementation(AndroidX.navigationRuntime)
+
+}
