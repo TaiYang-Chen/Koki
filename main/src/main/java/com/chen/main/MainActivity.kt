9fe33853a7961188851efadc6ee693de6fffd5c6
‘user’ <1563011912@qq.com>
2024-04-07 23:21:58 +0800

    [#7] Activity隐藏标题栏

--- main/src/main/java/com/chen/main/MainActivity.kt
+++ main/src/main/java/com/chen/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.chen.main
 
+import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.chen.base.activity.BaseActivity
 import com.chen.base.vm.BaseViewModel
 import com.chen.main.databinding.ActivityMainBinding
+import com.chen.base.ext.fixSystemUI
 
 /**
  * Author: Chen
@@ -14,5 +16,7 @@ import com.chen.main.databinding.ActivityMainBinding
 
 @Route(path = "/main/mainActivity")
 class MainActivity : BaseActivity<BaseViewModel, ActivityMainBinding>() {
-
+    override fun initView(savedInstanceState: Bundle?) {
+        fixSystemUI()
+    }
 }
